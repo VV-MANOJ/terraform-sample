335c58ca1253b26cc8609a1ba299e31349c4a122
M@nojrpms <30459831+VV-MANOJ@users.noreply.github.com>
2023-05-01 01:36:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -76,6 +76,13 @@ resource "aws_security_group" "web-sg" {
   }
 }
 
+
 output "web-address" {
   value = "${aws_instance.web.public_dns}:8080"
+}
+
+
+resource "aws_s3_bucket" "example" {
+  bucket = "shoaibmanojtesting-bucket"
+  acl    = "private"
 }
